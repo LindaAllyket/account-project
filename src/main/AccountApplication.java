72b93c679dbcb98db0dda6ff5cf42635abb0f5ee
LindaAllyket <Linda.Biasua@academytrainee.com>
2019-05-21 12:26:47 +0100

    bloop

--- src/main/AccountApplication.java
+++ src/main/AccountApplication.java
@@ -1,6 +1,17 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AccountApplication {
+	
+	static Map<Integer, Account> accounts = new HashMap<>();
+	
+	
+	 public AccountApplication(){
+		 
+	 }
+	 
 	 public void printingSentence() {
 		 System.out.println("Hello world to the standard out");
 	 }
